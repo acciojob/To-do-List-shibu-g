bd34bcbbf044ec8fde7f0d103b5c2103ddc7505d
shibu-g <109884948+shibu-g@users.noreply.github.com>
2023-02-14 21:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+var to=document.getElementById('inp');
+var btn=documet.getElementById('btn');
+var ol=document.getElementById('list');
+function add() {
+if(to.value){
+	var li=document.createElement('li');
+	li.textContent=to.value;
+	ol.appendChild(li);
+	to.value="";
+}
+}
+btn.addEventListener('click',add);
